932bce910ff7276d3c6a9b03118d0259bf1469ff
Natã Elienai <nata.elienai@gmail.com>
2023-02-15 20:11:38 -0300

    feat: add migration to populate manufacturer table

---
+++ src/main/resources/db/migration/V5__populate_manufacturer_table.sql
@@ -0,0 +1,12 @@
+INSERT INTO manufacturer (name)
+VALUES
+  ('EMS'),
+  ('Aché'),
+  ('Eurofarma'),
+  ('Sanofi'),
+  ('Johnson & Johnson'),
+  ('Takeda'),
+  ('Libbs'),
+  ('Neo Química'),
+  ('Mantecorp'),
+  ('P&G');
