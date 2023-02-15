8b8af825248039e47272539c2b3421bd94fb4ce2
Natã Elienai <nata.elienai@gmail.com>
2023-02-15 14:43:10 -0300

    chore: generate a spring boot project

---
+++ src/main/java/br/com/memory/projetoavaliacao/ProjetoAvaliacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.memory.projetoavaliacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAvaliacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAvaliacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/memory/projetoavaliacao/ProjetoAvaliacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.memory.projetoavaliacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAvaliacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
