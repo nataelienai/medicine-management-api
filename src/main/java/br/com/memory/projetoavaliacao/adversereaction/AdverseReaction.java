6cf347e3458b1d48c03e2588ec0f2d6ae05aaf47
Natã Elienai <nata.elienai@gmail.com>
2023-02-15 21:41:03 -0300

    feat: add adverse reaction entity

---
+++ src/main/java/br/com/memory/projetoavaliacao/adversereaction/AdverseReaction.java
@@ -0,0 +1,27 @@
+package br.com.memory.projetoavaliacao.adversereaction;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class AdverseReaction {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String description;
+
+  public AdverseReaction(String description) {
+    this.description = description;
+  }
+}
