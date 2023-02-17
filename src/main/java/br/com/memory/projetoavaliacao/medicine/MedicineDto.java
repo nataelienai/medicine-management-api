daa2581cf7b67e493cd34017a14250a8ec089c76
Natã Elienai <nata.elienai@gmail.com>
2023-02-17 16:38:31 -0300

    feat: add medicine dto

---
+++ src/main/java/br/com/memory/projetoavaliacao/medicine/MedicineDto.java
@@ -0,0 +1,23 @@
+package br.com.memory.projetoavaliacao.medicine;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MedicineDto {
+  private String registrationNumber;
+  private String name;
+  private LocalDate expirationDate;
+  private String customerServicePhone;
+  private BigDecimal price;
+  private Integer amountOfPills;
+  private Long manufacturerId;
+  private Set<Long> adverseReactionIds;
+}
