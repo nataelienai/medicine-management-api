5ea1d7dfaee019a22231b529b482a6dbfc70a0d1
Natã Elienai <nata.elienai@gmail.com>
2023-02-18 16:15:43 -0300

    feat: add anvisa registration number validator

---
+++ src/main/java/br/com/memory/projetoavaliacao/shared/validation/AnvisaRegistrationNumberValidator.java
@@ -0,0 +1,14 @@
+package br.com.memory.projetoavaliacao.shared.validation;
+
+import java.util.regex.Pattern;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class AnvisaRegistrationNumberValidator
+    implements ConstraintValidator<AnvisaRegistrationNumber, String> {
+  @Override
+  public boolean isValid(String value, ConstraintValidatorContext context) {
+    return Pattern.matches("^\\d\\.\\d{4}\\.\\d{4}\\.\\d{3}-\\d$", value);
+  }
+}
