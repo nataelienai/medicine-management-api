29818197643b7098656c016c2e9d01fea0555752
Natã Elienai <nata.elienai@gmail.com>
2023-02-18 14:29:12 -0300

    feat: add the find all medicines controller

---
+++ src/main/java/br/com/memory/projetoavaliacao/medicine/MedicineController.java
@@ -0,0 +1,29 @@
+package br.com.memory.projetoavaliacao.medicine;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/medicines")
+public class MedicineController {
+  private final MedicineService medicineService;
+
+  @GetMapping
+  @ResponseStatus(HttpStatus.OK)
+  public Page<Medicine> findAllBy(
+      @RequestParam(required = false) String registrationNumber,
+      @RequestParam(required = false) String name,
+      @PageableDefault Pageable pageable) {
+    return medicineService.findAllBy(registrationNumber, name, pageable);
+  }
+}
