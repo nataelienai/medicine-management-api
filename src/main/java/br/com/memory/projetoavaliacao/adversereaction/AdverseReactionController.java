dd4d393becb186655f8d1bff1975e49708e56669
Natã Elienai <nata.elienai@gmail.com>
2023-02-18 13:41:05 -0300

    feat: add the delete adverse reaction controller

--- src/main/java/br/com/memory/projetoavaliacao/adversereaction/AdverseReactionController.java
+++ src/main/java/br/com/memory/projetoavaliacao/adversereaction/AdverseReactionController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,5 +43,11 @@ public class AdverseReactionController {
   @ResponseStatus(HttpStatus.OK)
   public AdverseReaction update(@PathVariable Long id, @Valid @RequestBody AdverseReactionDto adverseReactionDto) {
     return adverseReactionService.update(id, adverseReactionDto);
+  }
+
+  @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteById(@PathVariable Long id) {
+    adverseReactionService.deleteById(id);
   }
 }
