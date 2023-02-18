bba4ef0f5936666d2f5d861de2eeac24f0c0ab64
Natã Elienai <nata.elienai@gmail.com>
2023-02-18 13:36:09 -0300

    feat: add the update adverse reaction controller

--- src/main/java/br/com/memory/projetoavaliacao/adversereaction/AdverseReactionController.java
+++ src/main/java/br/com/memory/projetoavaliacao/adversereaction/AdverseReactionController.java
@@ -7,7 +7,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -34,5 +36,11 @@ public class AdverseReactionController {
   @ResponseStatus(HttpStatus.CREATED)
   public AdverseReaction create(@Valid @RequestBody AdverseReactionDto adverseReactionDto) {
     return adverseReactionService.create(adverseReactionDto);
+  }
+
+  @PutMapping("/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public AdverseReaction update(@PathVariable Long id, @Valid @RequestBody AdverseReactionDto adverseReactionDto) {
+    return adverseReactionService.update(id, adverseReactionDto);
   }
 }
