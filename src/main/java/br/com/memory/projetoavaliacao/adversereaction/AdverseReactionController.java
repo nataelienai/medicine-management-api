842ee9c0f068da02ecd443838f55789531151419
Natã Elienai <nata.elienai@gmail.com>
2023-02-18 12:53:07 -0300

    feat: add the find all adverse reactions controller

---
+++ src/main/java/br/com/memory/projetoavaliacao/adversereaction/AdverseReactionController.java
@@ -0,0 +1,28 @@
+package br.com.memory.projetoavaliacao.adversereaction;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/adverse-reactions")
+public class AdverseReactionController {
+  private final AdverseReactionService adverseReactionService;
+
+  @GetMapping
+  @ResponseStatus(HttpStatus.OK)
+  public Page<AdverseReaction> findAllBy(
+      @RequestParam(required = false) String description,
+      @PageableDefault Pageable pageable) {
+    return adverseReactionService.findAllBy(description, pageable);
+  }
+}
