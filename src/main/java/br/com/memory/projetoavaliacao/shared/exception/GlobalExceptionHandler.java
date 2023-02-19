de368a864181770c2fe300f7d2f0e357265c352a
Natã Elienai <nata.elienai@gmail.com>
2023-02-18 22:57:59 -0300

    feat: add InvalidFormatException handling

--- src/main/java/br/com/memory/projetoavaliacao/shared/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/memory/projetoavaliacao/shared/exception/GlobalExceptionHandler.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.fasterxml.jackson.databind.JsonMappingException.Reference;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
   @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -22,4 +25,18 @@ public class GlobalExceptionHandler {
     return new ErrorResponse(statusCode, fieldError.getDefaultMessage());
   }
 
+  @ExceptionHandler(InvalidFormatException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ErrorResponse handleException(InvalidFormatException exception) {
+    Reference pathReference = exception.getPath().get(0);
+    int statusCode = HttpStatus.BAD_REQUEST.value();
+
+    if (pathReference == null) {
+      return new ErrorResponse(statusCode, "One of the provided fields has an invalid type or format");
+    }
+
+    String problematicField = pathReference.getFieldName();
+    String message = String.format("The %s field has an invalid type or format", problematicField);
+    return new ErrorResponse(statusCode, message);
+  }
 }
