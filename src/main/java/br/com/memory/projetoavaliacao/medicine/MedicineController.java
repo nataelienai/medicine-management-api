a4922546b96f2ac835623cbdebbd950b15695ee3
Natã Elienai <nata.elienai@gmail.com>
2023-02-18 21:05:07 -0300

    feat: add the create medicine controller

--- src/main/java/br/com/memory/projetoavaliacao/medicine/MedicineController.java
+++ src/main/java/br/com/memory/projetoavaliacao/medicine/MedicineController.java
@@ -1,10 +1,14 @@
 package br.com.memory.projetoavaliacao.medicine;
 
+import javax.validation.Valid;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -25,5 +29,11 @@ public class MedicineController {
       @RequestParam(required = false) String name,
       @PageableDefault Pageable pageable) {
     return medicineService.findAllBy(registrationNumber, name, pageable);
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Medicine create(@Valid @RequestBody MedicineCreationDto medicineCreationDto) {
+    return medicineService.create(medicineCreationDto);
   }
 }
