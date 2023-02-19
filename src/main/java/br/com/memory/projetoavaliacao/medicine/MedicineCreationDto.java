6fb886560e8c65477128e3e7c610453bf693aac5
Natã Elienai <nata.elienai@gmail.com>
2023-02-18 21:04:47 -0300

    feat: add validation constraints to medicine creation dto

--- src/main/java/br/com/memory/projetoavaliacao/medicine/MedicineCreationDto.java
+++ src/main/java/br/com/memory/projetoavaliacao/medicine/MedicineCreationDto.java
@@ -4,6 +4,15 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Set;
 
+import javax.validation.GroupSequence;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+
+import br.com.memory.projetoavaliacao.shared.validation.AnvisaRegistrationNumber;
+import br.com.memory.projetoavaliacao.shared.validation.Extended;
+import br.com.memory.projetoavaliacao.shared.validation.PhoneNumber;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,13 +20,33 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@GroupSequence({ MedicineCreationDto.class, Extended.class })
 public class MedicineCreationDto {
+  @NotBlank(message = "The registrationNumber field must not be blank")
+  @AnvisaRegistrationNumber(message = "The field registrationNumber has an invalid ANVISA registration number", groups = Extended.class)
   private String registrationNumber;
+
+  @NotBlank(message = "The name field must not be blank")
   private String name;
+
+  @NotNull(message = "The expirationDate field must not be null")
   private LocalDate expirationDate;
+
+  @NotBlank(message = "The customerServicePhone field must not be blank")
+  @PhoneNumber(message = "The field customerServicePhone has an invalid phone number format", groups = Extended.class)
   private String customerServicePhone;
+
+  @NotNull(message = "The price field must not be null")
+  @PositiveOrZero(message = "The price field must not be negative")
   private BigDecimal price;
+
+  @NotNull(message = "The amountOfPills field must not be null")
+  @PositiveOrZero(message = "The amountOfPills field must not be negative")
   private Integer amountOfPills;
+
+  @NotNull(message = "The manufacturerId field must not be null")
   private Long manufacturerId;
+
+  @NotEmpty(message = "The adverseReactionIds field must not be null nor empty")
   private Set<Long> adverseReactionIds;
 }
