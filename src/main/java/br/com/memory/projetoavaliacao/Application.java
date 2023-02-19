07a1b8cb566388bfa87f3259200e34bcd9025b9a
Natã Elienai <nata.elienai@gmail.com>
2023-02-19 18:32:42 -0300

    docs: annotate application with @OpenAPIDefinition

--- src/main/java/br/com/memory/projetoavaliacao/Application.java
+++ src/main/java/br/com/memory/projetoavaliacao/Application.java
@@ -3,6 +3,23 @@ package br.com.memory.projetoavaliacao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Medicine Management API",
+		version = "0.0.1",
+		description = "API that supports CRUD operations for medicines, manufacturers and adverse reactions."
+	),
+	servers = {
+		@Server(
+			description = "Local Server URL",
+			url = "http://localhost:8080"
+		)
+	}
+)
 @SpringBootApplication
 public class Application {
 
