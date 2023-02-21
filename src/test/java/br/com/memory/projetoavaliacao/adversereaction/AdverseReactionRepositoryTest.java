87e4f8d91793d7a7d925804a689c39a1a2835e7f
Natã Elienai <nata.elienai@gmail.com>
2023-02-20 22:43:21 -0300

    test: add case for adverse reaction repository findAllBy method

--- src/test/java/br/com/memory/projetoavaliacao/adversereaction/AdverseReactionRepositoryTest.java
+++ src/test/java/br/com/memory/projetoavaliacao/adversereaction/AdverseReactionRepositoryTest.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @DataJpaTest
 public class AdverseReactionRepositoryTest {
@@ -37,5 +39,26 @@ public class AdverseReactionRepositoryTest {
     // then
     assertThat(retrievedAdverseReactions.getTotalElements()).isEqualTo(adverseReactions.size());
     assertThat(retrievedAdverseReactions.getNumberOfElements()).isEqualTo(adverseReactions.size());
+  }
+
+  @Test
+  @DisplayName("findAllBy() should return a paged list of adverse reactions when given only pagination")
+  void findAllByShouldReturnPagedListOfAdverseReactionsWhenGivenOnlyPagination() {
+    // given
+    Set<AdverseReaction> adverseReactions = Set.of(
+        new AdverseReaction("Bad reaction"),
+        new AdverseReaction("Very bad reaction"),
+        new AdverseReaction("Worst reaction"));
+    adverseReactionRepository.saveAll(adverseReactions);
+
+    // when
+    Pageable firstPage = PageRequest.of(0, 2);
+    Page<AdverseReaction> pagedAdverseReactions = adverseReactionRepository.findAllBy(null, firstPage);
+
+    // then
+    assertThat(pagedAdverseReactions.getTotalElements()).isEqualTo(adverseReactions.size());
+    assertThat(pagedAdverseReactions.getNumberOfElements()).isEqualTo(firstPage.getPageSize());
+    assertThat(pagedAdverseReactions.getSize()).isEqualTo(firstPage.getPageSize());
+    assertThat(pagedAdverseReactions.getNumber()).isEqualTo(firstPage.getPageNumber());
   }
 }
