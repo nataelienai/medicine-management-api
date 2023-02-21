8709ee83ce408e0a9191806a33f252395f4a8de3
Natã Elienai <nata.elienai@gmail.com>
2023-02-21 20:12:29 -0300

    test: add case for medicine service create method

--- src/test/java/br/com/memory/projetoavaliacao/medicine/MedicineServiceTest.java
+++ src/test/java/br/com/memory/projetoavaliacao/medicine/MedicineServiceTest.java
@@ -1,6 +1,8 @@
 package br.com.memory.projetoavaliacao.medicine;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
@@ -21,6 +23,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+import br.com.memory.projetoavaliacao.adversereaction.AdverseReaction;
 import br.com.memory.projetoavaliacao.adversereaction.AdverseReactionRepository;
 import br.com.memory.projetoavaliacao.manufacturer.Manufacturer;
 import br.com.memory.projetoavaliacao.manufacturer.ManufacturerRepository;
@@ -140,5 +143,43 @@ public class MedicineServiceTest {
         .isInstanceOf(ResourceNotFoundException.class);
 
     verify(medicineRepository, never()).save(any());
+  }
+
+  @Test
+  @DisplayName("create() should create a medicine when given an entirely valid input")
+  void createShouldCreateMedicineWhenGivenEntirelyValidInput() {
+    // given
+    Manufacturer manufacturer = new Manufacturer(1L, "manufacturer");
+    AdverseReaction adverseReaction = new AdverseReaction(1L, "description");
+    MedicineCreationDto medicineCreationDto = new MedicineCreationDto(
+        "1.4444.4444.333-1",
+        "medicine",
+        LocalDate.now(),
+        "(12)0000-0000",
+        BigDecimal.valueOf(1),
+        1,
+        manufacturer.getId(),
+        Set.of(adverseReaction.getId()));
+    when(medicineRepository.existsById(medicineCreationDto.getRegistrationNumber()))
+        .thenReturn(false);
+    when(manufacturerRepository.findById(medicineCreationDto.getManufacturerId()))
+        .thenReturn(Optional.of(manufacturer));
+    when(adverseReactionRepository.findAllById(medicineCreationDto.getAdverseReactionIds()))
+        .thenReturn(List.of(adverseReaction));
+    when(medicineRepository.save(any(Medicine.class))).then(returnsFirstArg());
+
+    // when
+    Medicine medicine = medicineService.create(medicineCreationDto);
+
+    // then
+    assertThat(medicine).isNotNull();
+    assertThat(medicine.getRegistrationNumber()).isEqualTo(medicineCreationDto.getRegistrationNumber());
+    assertThat(medicine.getName()).isEqualTo(medicineCreationDto.getName());
+    assertThat(medicine.getExpirationDate()).isEqualTo(medicineCreationDto.getExpirationDate());
+    assertThat(medicine.getCustomerServicePhone()).isEqualTo(medicineCreationDto.getCustomerServicePhone());
+    assertThat(medicine.getPrice()).isEqualTo(medicineCreationDto.getPrice());
+    assertThat(medicine.getAmountOfPills()).isEqualTo(medicineCreationDto.getAmountOfPills());
+    assertThat(medicine.getManufacturer()).isEqualTo(manufacturer);
+    assertThat(medicine.getAdverseReactions().contains(adverseReaction)).isTrue();
   }
 }
