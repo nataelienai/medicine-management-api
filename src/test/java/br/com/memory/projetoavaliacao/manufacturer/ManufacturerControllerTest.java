edfaf446269eecd266466f436e8f306e90e433db
Natã Elienai <nata.elienai@gmail.com>
2023-02-22 01:55:30 -0300

    test: add case for manufacturer controller GET manufacturers endpoint

---
+++ src/test/java/br/com/memory/projetoavaliacao/manufacturer/ManufacturerControllerTest.java
@@ -0,0 +1,60 @@
+package br.com.memory.projetoavaliacao.manufacturer;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebMvcTest(controllers = ManufacturerController.class)
+public class ManufacturerControllerTest {
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @MockBean
+  private ManufacturerService manufacturerService;
+
+  @Test
+  @DisplayName("GET /manufacturers should return 200 and paged list of manufacturers when given filter and pagination")
+  void getManufacturersShouldReturn200AndPagedListOfManufacturersWhenGivenFilterAndPagination() throws Exception {
+    // given
+    List<Manufacturer> manufacturers = List.of(
+        new Manufacturer(1L, "Manufacturer"));
+    Pageable pageable = PageRequest.of(0, 10);
+    Page<Manufacturer> pagedManufacturers = new PageImpl<>(
+        manufacturers,
+        pageable,
+        manufacturers.size());
+
+    when(manufacturerService.findAllBy("manu", pageable))
+        .thenReturn(pagedManufacturers);
+
+    // when
+    // then
+    String expected = objectMapper.writeValueAsString(pagedManufacturers);
+    mockMvc.perform(get("/manufacturers")
+        .param("name", "manu")
+        .param("page", "0")
+        .param("size", "10"))
+        .andExpect(status().isOk())
+        .andExpect(content().string(expected));
+  }
+}
