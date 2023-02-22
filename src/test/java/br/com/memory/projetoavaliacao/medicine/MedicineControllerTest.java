5fbebe7955362f6f4ec554da25fae94de9b57f3b
Natã Elienai <nata.elienai@gmail.com>
2023-02-22 18:20:16 -0300

    test: add failure case for PUT medicines controller

--- src/test/java/br/com/memory/projetoavaliacao/medicine/MedicineControllerTest.java
+++ src/test/java/br/com/memory/projetoavaliacao/medicine/MedicineControllerTest.java
@@ -562,6 +562,27 @@ public class MedicineControllerTest {
   }
 
   @Test
+  @DisplayName("PUT /medicines/{registrationNumber} should return 400 when given a null expiration date")
+  void putMedicinesShouldReturn400WhenGivenNullExpirationDate() throws Exception {
+    // given
+    MedicineUpdateDto medicineUpdateDto = new MedicineUpdateDto(
+        "medicine",
+        null,
+        "(12)0000-0000",
+        BigDecimal.valueOf(1),
+        1,
+        1L,
+        Set.of(1L));
+
+    // when
+    // then
+    assertThatPutMethodReturns400(
+        "1.4444.4444.333-1",
+        medicineUpdateDto,
+        "The expirationDate field must not be null");
+  }
+
+  @Test
   @DisplayName("PUT /medicines/{registrationNumber} should return 404 when registration number is not found")
   void putMedicinesShouldReturn404WhenRegistrationNumberIsNotFound() throws Exception {
     // given
