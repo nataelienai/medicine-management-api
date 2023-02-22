3318fddbf5ce1f131b91a1a200f088ca138b315a
Natã Elienai <nata.elienai@gmail.com>
2023-02-22 16:49:31 -0300

    test: add failure case for POST medicines controller

--- src/test/java/br/com/memory/projetoavaliacao/medicine/MedicineControllerTest.java
+++ src/test/java/br/com/memory/projetoavaliacao/medicine/MedicineControllerTest.java
@@ -352,6 +352,26 @@ public class MedicineControllerTest {
   }
 
   @Test
+  @DisplayName("POST /medicines should return 400 when given a negative amount of pills")
+  void postMedicinesShouldReturn400WhenGivenNegativeAmountOfPills() throws Exception {
+    // given
+    MedicineCreationDto medicineCreationDto = new MedicineCreationDto(
+        "1.4444.4444.333-1",
+        "medicine",
+        LocalDate.now(),
+        "(12)0000-0000",
+        BigDecimal.valueOf(1),
+        -1,
+        1L,
+        Set.of(1L));
+    // when
+    // then
+    assertThatPostMethodReturns400(
+        medicineCreationDto,
+        "The amountOfPills field must not be negative");
+  }
+
+  @Test
   @DisplayName("DELETE /medicines/{registrationNumber} should return 204 when given valid registration number")
   void deleteMedicineShouldReturn204WhenGivenValidRegistrationNumber() throws Exception {
     // given
