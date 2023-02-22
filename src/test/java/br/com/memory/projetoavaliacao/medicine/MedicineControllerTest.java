852567d907fad6d484abb40dea4eb7f4b91aa378
Natã Elienai <nata.elienai@gmail.com>
2023-02-22 18:30:00 -0300

    test: add failure case for PUT medicines controller

--- src/test/java/br/com/memory/projetoavaliacao/medicine/MedicineControllerTest.java
+++ src/test/java/br/com/memory/projetoavaliacao/medicine/MedicineControllerTest.java
@@ -625,6 +625,27 @@ public class MedicineControllerTest {
   }
 
   @Test
+  @DisplayName("PUT /medicines/{registrationNumber} should return 400 when given an invalid customer service phone")
+  void putMedicinesShouldReturn400WhenGivenInvalidCustomerServicePhone() throws Exception {
+    // given
+    MedicineUpdateDto medicineUpdateDto = new MedicineUpdateDto(
+        "medicine",
+        LocalDate.now(),
+        "123123",
+        BigDecimal.valueOf(1),
+        1,
+        1L,
+        Set.of(1L));
+
+    // when
+    // then
+    assertThatPutMethodReturns400(
+        "1.4444.4444.333-1",
+        medicineUpdateDto,
+        "The customerServicePhone field has an invalid phone number format");
+  }
+
+  @Test
   @DisplayName("PUT /medicines/{registrationNumber} should return 404 when registration number is not found")
   void putMedicinesShouldReturn404WhenRegistrationNumberIsNotFound() throws Exception {
     // given
